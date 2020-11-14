3cbaaa54230db12022d68d98141a0cb9f0a52d7d
Mark Wallsgrove <markwallsgrove@users.noreply.github.com>
2020-11-14 12:10:40 +0000

    Add parse command function

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hello');
+
+
+/**
+ * Parse the command line argument into an array. The first five entries will be the time patterns,
+ * which will be followed by the command. These values are not validated.
+ * @param {string} arg Argument from the terminal
+ * @return string[] array containing the time patterns followed by the command
+ */
+const parseCmdArgument = (arg) => {
+    const chunks = arg.split(' ');
+    const timePatterns = chunks.slice(0, 5);
+    const command = chunks.slice(5).join(' ');
+    return [...timePatterns, command];
+};
+
+
+module.exports.parseCmdArgument = parseCmdArgument;
--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,16 @@
-test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(3);
+const { parseCmdArgument } = require('../index');
+
+test('should parse command argument', () => {
+  const chunks = parseCmdArgument('*/15 0 1,15 * 1-5 /usr/bin/find');
+  expect(chunks).toStrictEqual(['*/15', '0', '1,15', '*', '1-5', '/usr/bin/find']);
 });
+
+test('should parse command argument when command has arguments', () => {
+  const chunks = parseCmdArgument('*/15 0 1,15 * 1-5 /usr/bin/find . -type f');
+  expect(chunks).toStrictEqual(['*/15', '0', '1,15', '*', '1-5', '/usr/bin/find . -type f']);
+});
+
+test('should parse command argument when command is empty', () => {
+    const chunks = parseCmdArgument('');
+    expect(chunks).toStrictEqual(['', '']);
+  });
